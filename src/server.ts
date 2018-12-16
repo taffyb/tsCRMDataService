4a2515596fc9721c1c971de98ff3e9eb627e16a7
Taffy Brecknock <brecknockt@gmail.com>
2018-12-16 14:01:39 +0000

    setup

---
+++ src/server.ts
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+
+ import * as Express from "express";
+import {ServerLoader, ServerSettings} from "@tsed/common";
+import Path = require("path");
+ 
+@ServerSettings({
+    rootDir: Path.resolve(__dirname),
+    acceptMimes: ["application/json"]
+})
+export class Server extends ServerLoader {
+ 
+    /**
+     * This method let you configure the middleware required by your application to works.
+     * @returns {Server} 
+     */
+    public $onMountingMiddlewares(): void|Promise<any> {
+    
+        const cookieParser = require('cookie-parser'),
+            bodyParser = require('body-parser'),
+            compress = require('compression'),
+            methodOverride = require('method-override');
+ 
+ 
+        this
+            .use(GlobalAcceptMimesMiddleware)
+            .use(cookieParser())
+            .use(compress({}))
+            .use(methodOverride())
+            .use(bodyParser.json())
+            .use(bodyParser.urlencoded({
+                extended: true
+            }));
+ 
+        return null;
+    }
+ 
+    public $onReady(){
+        console.log('Server started...');
+    }
+   
+    public $onServerInitError(err){
+        console.error(err);
+    }    
+}
+ 
+new Server().start();
